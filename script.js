03c57f95773f1b42f32dad0f6b51f0555d2ae256
Ahmed Hemida <72874042+Ahmed-Hemida@users.noreply.github.com>
2023-10-17 10:05:17 +0300

    prind list

--- script.js
+++ script.js
@@ -3,3 +3,4 @@ document.querySelectorAll("#items #video-title").forEach(function (element) {
   var textContent = element.textContent.replace(/\r?\n|\r/g, '').trim();
   data.push(textContent);
 });
+console.log(data)
